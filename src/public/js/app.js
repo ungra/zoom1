b860b360edad860cdb8b7dc0efe2921a00a127eb
junhee_lee2 <junhee_lee2@tmax.co.kr>
2024-10-05 17:54:19 +0900

    3.0 User video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,85 +1,48 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const room = document.getElementById("room");
-const form = welcome.querySelector("form");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-function addMessage(msg) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = msg;
-  ul.appendChild(li);
-}
-
-function handleMessageSubmit(event) {
-  event.preventDefault();
-  const msgInput = room.querySelector("#msg input");
-  const value = msgInput.value;
-  socket.emit("new_message_from_client", msgInput.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-}
-
-function handleNicknameSubmit(event) {
-  event.preventDefault();
-  const nicknameInput = room.querySelector("#nickname input");
-  socket.emit("nickname", nicknameInput.value, roomName);
-}
-
-function showRoom(roomCount) {
-  welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room Name : ${roomName} ${roomCount}`;
-  const msgForm = room.querySelector("#msg");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-  const nicknameForm = room.querySelector("#nickname");
-  nicknameForm.addEventListener("submit", handleNicknameSubmit);
-}
-
-function handleRoomSubmit(event) {
-  event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-}
-
-form.addEventListener("submit", handleRoomSubmit);
-
-socket.on("welcome", (nickname, roomCount) => {
-  console.log("welcome");
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room Name : ${roomName} ${roomCount}`;
-  console.log(roomCount);
-  addMessage(`${nickname} joined!`);
-});
-
-socket.on("new_message_from_server", addMessage);
-
-socket.on("bye", (nickname, roomCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room Name : ${roomName} ${roomCount}`;
-  addMessage(`${nickname} left!!`);
-});
-
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
   }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.appendChild(li);
-  });
-});
+}
+
+getMedia();
+
+function handleMuteBtnClick() {
+  if (muted) {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  } else {
+    muteBtn.innerText = "UnMute";
+    muted = true;
+  }
+}
+function handleCameraBtnClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = "Turn Off Camera";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn On Camera";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteBtnClick);
+cameraBtn.addEventListener("click", handleCameraBtnClick);
 
 //socket.io vs websocket
 //1) 어떤 event라도 보낼 수 있다.
