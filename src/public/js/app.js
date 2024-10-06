c7582a803e8a373d09dd841436c2a1f43787fa9b
junhee_lee2 <junhee_lee2@tmax.co.kr>
2024-10-06 20:59:21 +0900

    3.1 Call controls

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,46 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
+const audioSelect = document.getElementById("audios");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let devices;
+
+function getCameras() {
+  const cameras = devices.filter((device) => device.kind === "videoinput");
+  cameras.forEach((camera) => {
+    const option = document.createElement("option");
+    option.value = camera.deviceId;
+    option.innerText = camera.label;
+    cameraSelect.appendChild(option);
+  });
+}
+
+function getAudios() {
+  const audios = devices.filter((device) => {
+    return device.kind === "audioinput";
+  });
+  audios.forEach((audio) => {
+    const option = document.createElement("option");
+    option.value = audio.deviceId;
+    option.innerText = audio.label;
+    audioSelect.appendChild(option);
+  });
+}
 
 async function getMedia() {
   try {
+    devices = await navigator.mediaDevices.enumerateDevices();
     myStream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
     myFace.srcObject = myStream;
+    getCameras();
+    getAudios();
   } catch (e) {
     console.log(e);
   }
@@ -23,6 +51,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteBtnClick() {
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (muted) {
     muteBtn.innerText = "Mute";
     muted = false;
@@ -32,6 +63,9 @@ function handleMuteBtnClick() {
   }
 }
 function handleCameraBtnClick() {
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
   if (cameraOff) {
     cameraBtn.innerText = "Turn Off Camera";
     cameraOff = false;
