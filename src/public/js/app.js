7246e48dfa3bd00e62c4fec8fc38be587b1308d2
junhee_lee2 <junhee_lee2@tmax.co.kr>
2024-10-07 09:48:39 +0900

    3.2 Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,14 @@ let devices;
 
 function getCameras() {
   const cameras = devices.filter((device) => device.kind === "videoinput");
+  const currentCamera = myStream.getVideoTracks()[0];
   cameras.forEach((camera) => {
     const option = document.createElement("option");
     option.value = camera.deviceId;
     option.innerText = camera.label;
+    if (currentCamera.label === camera.label) {
+      option.selected = true;
+    }
     cameraSelect.appendChild(option);
   });
 }
@@ -25,24 +29,37 @@ function getAudios() {
   const audios = devices.filter((device) => {
     return device.kind === "audioinput";
   });
+  const currentAudio = myStream.getAudioTracks()[0];
   audios.forEach((audio) => {
     const option = document.createElement("option");
     option.value = audio.deviceId;
     option.innerText = audio.label;
+    if (currentAudio.label === audio.label) {
+      option.selected = true;
+    }
     audioSelect.appendChild(option);
   });
 }
 
-async function getMedia() {
+async function getMedia(cameraId, audioId) {
+  const initialConstraints = {
+    audio: true,
+    video: true,
+  };
+  const currentConstraints = {
+    audio: { deviceId: { exact: audioId } },
+    video: { deviceId: { exact: cameraId } },
+  };
   try {
     devices = await navigator.mediaDevices.enumerateDevices();
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      cameraId ? currentConstraints : initialConstraints
+    );
     myFace.srcObject = myStream;
-    getCameras();
-    getAudios();
+    if (!cameraId) {
+      getCameras();
+      getAudios();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -75,8 +92,18 @@ function handleCameraBtnClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(cameraSelect.value, audioSelect.value);
+}
+
+async function handleAudioChange() {
+  await getMedia(cameraSelect.value, audioSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteBtnClick);
 cameraBtn.addEventListener("click", handleCameraBtnClick);
+cameraSelect.addEventListener("input", handleCameraChange);
+audioSelect.addEventListener("input", handleAudioChange);
 
 //socket.io vs websocket
 //1) 어떤 event라도 보낼 수 있다.
